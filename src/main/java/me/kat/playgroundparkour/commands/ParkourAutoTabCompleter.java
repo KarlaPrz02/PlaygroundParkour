a58159ca74019b313e0163425a738e4a732190cd
Karla <imcarla2002@gmail.com>
2024-09-06 03:49:53 +0200

    Working on AutoTabCompleter (still in progress)

---
+++ src/main/java/me/kat/playgroundparkour/commands/ParkourAutoTabCompleter.java
@@ -0,0 +1,16 @@
+package me.kat.playgroundparkour.commands;
+
+import jdk.tools.jlink.internal.plugins.AbstractPlugin;
+import me.kat.playgroundparkour.ParkourManager
+import org.bukkit.command.TabCompleter;
+import java.util.ArrayList;
+import java.util.List;
+
+
+public class ParkourAutoTabCompleter extends AbstractPlugin implements TabCompleter {
+
+    private final public static void main(String[] args) {
+// me wa dormir luego sigo
+    }
+
+}
